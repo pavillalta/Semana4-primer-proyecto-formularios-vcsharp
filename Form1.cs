9195a1f83acaf9028caf56040ac528d39b092613
Pedro Villalta <pavblogs@gmail.com>
2021-08-18 11:07:34 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana4_primer_proyecto_formularios_vcsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
